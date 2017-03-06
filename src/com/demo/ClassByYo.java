5f20ab64de62ab5967a43c10feaef8a5b8787b40
yogymax <yogymax@gmail.com>
2017-03-06 18:49:25 +0530

    second commit

--- src/com/demo/ClassByYo.java
+++ src/com/demo/ClassByYo.java
@@ -3,7 +3,7 @@ package com.demo;
 public class ClassByYo {
 
 	public static void main(String[] args) {
-		System.out.println("How r u doing today...");
+		System.out.println("How r u doing today......");
 	}
 
 }
