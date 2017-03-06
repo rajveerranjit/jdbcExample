ff607c67cff98bc045176b2eb8aad0dbcafb5a8f
balpandeyogita <yogitabalpande@gmail.com>
2017-03-06 18:28:07 +0530

    third commit

--- src/com/demo/Hello.java
+++ src/com/demo/Hello.java
@@ -3,8 +3,7 @@ package com.demo;
 public class Hello {
 
 	public static void main(String[] args) {
-		System.out.println("Hello...");
-		System.out.println("yogita");
+		System.out.println("yo.....yo");
 		
 	}
 
